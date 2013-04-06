d44f2fdb5511ea974d10b679772b7135a0488753
Aaron <magwe25@gmail.com>
2013-04-06 16:24:53 -0400

    more bug fixes

--- events.js
+++ events.js
@@ -5,32 +5,41 @@ $(document).ready(function() {
 	canvasDraw();
 });
 function canvasDraw(){
-	field = Document.getElementById('field');
+	field = document.getElementById("field");
 	ctx = field.getContext('2d');
-	field.drawImage("field.png",0,0);
+	var fieldImage = new Image();
+	fieldImage.src = "field.png";
+	ctx.drawImage(fieldImage, 0, 0);
 }
 function login(e){
-	e.stopPropogation();
+	e.stopPropagation();
 	e.preventDefault();
-	$("#login").slideUp();
-	var theName = $('#login').serialize();
-	name = theName.name;
-	submitCharacter();
+	var theName = $('#login').serializeJSON();
+	console.log(theName);
+	if (theName.name == "")
+		alert("Please Type a Name, Douche.");
+	else{
+		name = theName.name;
+		submitCharacter();
+		$("#login").slideUp();
+	}
 }
 function submitCharacter(){
 	submit(name);
 }
-
+/*
 while(true){
 	//fetch
 	$json = getDrawData();
 	//draw
 	ctx.drawImage("ball.png",$json.ball.x, $json.ball.y);
 	$.each($json.red,function(index,value){
-		ctx.drawImage("red_" + value.action + ".png", value.x, value.ball.y);
-		ctx.fillText(value.id,x,y)
+		ctx.drawImage("red_" + value.action + ".png", value.x, value.y);
+		ctx.fillText(value.id,x,y);
 	});
 	$.each($json.blue,function(index,value){
-		ctx.drawImage("blue_" + value.action + ".png", value.x, value.ball.y);
+		ctx.drawImage("blue_" + value.action + ".png", value.x, value.y);
+		ctx.fillText(value.id,x,y);
 	});
 }
+*/
